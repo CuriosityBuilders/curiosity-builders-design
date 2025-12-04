d9b1cc891953da459d1a95bda9753122af9eb9c1
Félix Orain <felix.orain@gmail.com>
2025-12-04 15:12:20 -0800

    Refactor: Reorganized Sanity schema by moving page definitions to a dedicated 'pages' directory, removed deprecated service pages, and updated imports for improved structure and clarity.

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,19 +2,19 @@ import { type SchemaTypeDefinition } from "sanity";
 
 // Homepage - Single document
 import homepage from "./homepage/homepage";
-import footer from "./layout/footer";
-import header from "./layout/header";
 // Layout
+import header from "./layout/header";
+import footer from "./layout/footer";
 import navigation from "./layout/navigation";
-import contactPage from "./services/contactPage";
-import methodePage from "./services/methodePage";
-import servicesPage from "./services/servicesPage";
 // Shared
 import bookImage from "./shared/bookImage";
 import logos from "./shared/logos";
 import projectImages from "./shared/projectImages";
 // Pages
-import signalsPage from "./signals/signalsPage";
+import signalsPage from "./pages/signalsPage";
+import contactPage from "./pages/contactPage";
+import methodePage from "./pages/methodePage";
+import servicesPage from "./pages/servicesPage";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
